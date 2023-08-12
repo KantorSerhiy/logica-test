be1a2ba2fc8ebc4a4f80af698a2b472a0cd91d91
skantor <serhiy.kantor@gmail.com>
2023-08-12 14:00:08 +0300

    add main and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logica',
+    version='2.0.0',
+    author='PolicyEngine team',
+    author_email='',
+    url='',
+    description='logica',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
